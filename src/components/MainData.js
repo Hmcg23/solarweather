a85f8ae63191effb833a310e6e4ebc169ff6f34e
H_mcg <82006254+Hmcg23@users.noreply.github.com>
2022-08-16 22:51:06 -0700

    clear search bar after submit

--- src/components/MainData.js
+++ src/components/MainData.js
@@ -18,7 +18,7 @@ function MainData({selectedCityData, units}) {
                     <img src={cityData ? `https://openweathermap.org/img/wn/${cityData.list[0].weather[0].icon}.png` : ''} alt="weather icon" />
                     <h3>{cityData ? cityData.list[0].weather[0].description : ''}</h3>
                     <h2>Feels Like: {cityData ? Math.floor(cityData.list[0].main.feels_like) : ''}</h2>
-                    <h2>Humidity: {cityData ? Math.floor(cityData.list[0].main.humidity) : ''}</h2>
+                    <h2>Humidity: {cityData ? Math.floor(cityData.list[0].main.humidity) : ''}%</h2>
                     <h2>Wind Speed: {cityData ? Math.floor(cityData.list[0].wind.speed) : ''} {units === 'imperial' ? 'mph' : 'm/s'}</h2>                    
                 </div>
 
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -5,19 +5,17 @@ import { City }  from 'country-state-city';
 
 function SearchBar({getDataFromSearch}) {
   const [filteredData, setFilteredData] = useState([]);
+  const [searchValue, setSearchValue] = useState('');
 
-  const allCities = City.getAllCities()
+  const allCities = City.getAllCities();
+  
 
   const handleChange = (event) => {
-    const searchValue = event.target.value;
+    setSearchValue(event.target.value);
     const newFilter = allCities.filter(item => {
         return item.name.toLowerCase().includes(searchValue.toLowerCase()) || item.countryCode.toLowerCase().includes(searchValue.toLowerCase());
     });
-    if (searchValue === "") {
-        setFilteredData([]);
-    } else {
-        setFilteredData(newFilter);
-    }
+    setFilteredData(newFilter);
   }
 
 
@@ -27,14 +25,15 @@ function SearchBar({getDataFromSearch}) {
             <SearchIcon className="searchIcon"/>
             <h1>Search for any city</h1>
         </div>
-        <input type="text" placeholder=" London, GB" onChange={handleChange}/>
+        <input type="text" placeholder=" London, GB" onChange={handleChange} value={searchValue}/>
         {
             filteredData.length !== 0 && (
-                <div className="dataResult">
+                <div className="dataResult" style={searchValue === '' ? {display: 'none'} : {display: 'block'}}>
                     {
                     filteredData.slice(0, 15).map((city, id) => (
                         <p key={id} className="dataItem" onClick={() => {
                             getDataFromSearch(`${city.name}, ${city.countryCode}`, [parseFloat(city.latitude), parseFloat(city.longitude)]);
+                            setSearchValue('');
                         }}>{city.name}, {city.countryCode}</p>
                     ))
                     }
